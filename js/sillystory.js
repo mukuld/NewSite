e115f9acff7ac1e48170f323de0846c7fd3e33db
Mukul Dharwadkar <mukul.dharwadkar@gmail.com>
2024-10-21 22:06:38 -0700

    Declaring all variables and functions

--- js/sillystory.js
+++ js/sillystory.js
@@ -8,6 +8,7 @@ function randomValueFromArray(array){
 }
 
 const storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day."
+
 const insertX = `
 Willy the Goblin
 Big Daddy
@@ -27,18 +28,27 @@ turned into a slug and crawled away
 randomize.addEventListener('click', result);
 
 function result() {
+  
+  let newStory = storyText;
+  let xItem = randomValueFromArray(insertX);
+  let yItem = randomValueFromArray(insertY);
+  let zItem = randomValueFromArray(insertZ);
+
+  newStory = newStory.replace(":insertx", xItem);
 
   if(customName.value !== '') {
     const name = customName.value;
-
+    newStory = newStory.replace("Bob", name);
   }
 
   if(document.getElementById("uk").checked) {
-    const weight = Math.round(300);
-    const temperature =  Math.round(94);
+    const weight = Math.round(300 / 14) + " stone";
+    const temperature =  Math.round((94 - 32) * 5 / 9) + "celcius";
+    newStory = newStory.replace("300 pounds", weight);
+    newStory = newStory.replace("94 fahrenheit", temperature);
 
   }
 
-  story.textContent = ;
+  story.textContent = newStory;
   story.style.visibility = 'visible';
 } 
