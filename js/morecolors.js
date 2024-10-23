d3d0c19badbf2d31e97d7b54d3cca20256a6bc78
Mukul Dharwadkar <mukul.dharwadkar@gmail.com>
2024-10-23 15:19:18 -0700

    More colors webpage

---
+++ js/morecolors.js
@@ -0,0 +1,30 @@
+const select = document.querySelector('select');
+const html = document.querySelector('.output');
+
+select.addEventListener('change', () => {
+  const choice = select.value;
+
+  // ADD SWITCH STATEMENT
+  switch (choice) {
+  case "white":
+	update("white", "black");
+	break;
+  case "black":
+	update("black", "white");
+	break;
+  case "purple":
+	update("purple", "white");
+	break;
+  case "yellow":
+	update("yellow", "blue");
+	break;
+  case "psychedelic":
+	update("green", "yellow");
+	break;
+}
+});
+
+function update(bgColor, textColor) {
+  html.style.backgroundColor = bgColor;
+  html.style.color = textColor;
+}
