3caa8055e692a6c8c3bffcaf3e88e5efd1194e51
Mukul Dharwadkar <mukul.dharwadkar@gmail.com>
2024-10-25 13:34:45 -0700

    new content to create a list of perfect squares

---
+++ js/skipsquares.js
@@ -0,0 +1,23 @@
+/* 
+* Javascript to generate a list of numbers that are perfect squares.
+* Date: 25 October 2024
+* Programmer: Mukul Dharwadkar
+*/
+
+const para = document.querySelector("p");
+const input = document.querySelector("input");
+const btn = document.querySelector("button");
+
+btn.addEventListener("click", () => {
+    para.textContent = "Output: ";
+    const num = input.value;
+    input.value = "";
+    input.focus();
+    for (let i = 0; i <= num; i++) {
+        let sqRoot = Math.sqrt(i);
+        if (Math.floor(sqRoot) !== sqRoot) {
+            continue;
+        }
+        para.textContent += `${i} `;
+    }
+});
