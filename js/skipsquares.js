c3e9ea55b17a2a5433f90964e807bd5f2820e04c
Mukul Dharwadkar <mukul.dharwadkar@gmail.com>
2024-10-25 13:38:54 -0700

    Log the value of sqRoot

--- js/skipsquares.js
+++ js/skipsquares.js
@@ -15,6 +15,7 @@ btn.addEventListener("click", () => {
     input.focus();
     for (let i = 1; i <= num; i++) {
         let sqRoot = Math.sqrt(i);
+        console.log(sqRoot);
         if (Math.floor(sqRoot) !== sqRoot) {
             continue;
         }
