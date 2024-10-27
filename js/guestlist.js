c832a2f79597a3d67b6596960b7d8864e2f2f0ef
Mukul Dharwadkar <mukul.dharwadkar@gmail.com>
2024-10-26 20:12:42 -0700

    New Guest List content

---
+++ js/guestlist.js
@@ -0,0 +1,21 @@
+/* 
+* Javascript to generate a list of numbers that are perfect squares.
+* Date: 26 October 2024
+* Programmer: Mukul Dharwadkar
+*/
+const people = ['Chris', 'Anne', 'Colin', 'Terri', 'Phil', 'Lola', 'Sam', 'Kay', 'Bruce'];
+
+const admitted = document.querySelector('.admitted');
+const refused = document.querySelector('.refused');
+admitted.textContent = 'Admit: ';
+refused.textContent = 'Refuse: ';
+
+for (person of people) {
+    if (person === "Phil" || "Lola") {
+        refused.textContent += person;
+
+    } else {
+        admitted.textContent += person;
+    }
+}
+
