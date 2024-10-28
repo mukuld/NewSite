d514253abae69280faf8ac74a925a3093f98459b
Mukul Dharwadkar <mukul.dharwadkar@gmail.com>
2024-10-28 16:29:38 -0700

    Updated left padding and message type

--- js/byofn.js
+++ js/byofn.js
@@ -5,7 +5,7 @@
 */
 
 const btn = document.querySelector("button");
-btn.addEventListener("click", () => displayMessage("This is a different message", "warning"),);
+btn.addEventListener("click", () => displayMessage("This is a different message", "chat"),);
 
 function displayMessage(msgText, msgType) {
     const body = document.body;
