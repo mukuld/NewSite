d76bdd7080233e02fb2ed4b655d012ad208b188d
Mukul Dharwadkar <mukul.dharwadkar@gmail.com>
2024-10-28 13:59:42 -0700

    Code and updates to css and index for BYOFN

---
+++ js/byofn.js
@@ -0,0 +1,37 @@
+/* 
+* Javascript to demonstrate custom functions.
+* Date: 28 October 2024
+* Programmer: Mukul Dharwadkar
+*/
+
+const btn = document.querySelector("button");
+btn.addEventListener("click", () => displayMessage("This is a different message", "warning"),);
+
+function displayMessage(msgText, msgType) {
+    const body = document.body;
+
+    const panel = document.createElement("div");
+    panel.setAttribute("class", "msgBox");
+    body.appendChild(panel);
+
+    const msg = document.createElement("p");
+    msg.textContent = msgText;
+    panel.appendChild(msg);
+
+    const closeBtn = document.createElement("button");
+    closeBtn.textContent = "x";
+    panel.appendChild(closeBtn);
+
+    closeBtn.addEventListener("click", () => 
+    panel.parentNode.removeChild(panel),);
+
+    if (msgType === "warning") {
+        msg.style.backgroundImage = "url(img/warning.png)";
+        panel.style.backgroundColor = "red";
+    } else if (msgType === "chat") {
+        msg.style.backgroundImage = "url(img/chat.png)";
+        panel.style.backgroundColor = "aqua";
+    } else {
+        msg.style.paddingLeft = "20px";
+    }
+}
