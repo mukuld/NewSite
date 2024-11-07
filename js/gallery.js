97785c4f20df22b0a48764480b917a0ecaa755e4
Mukul Dharwadkar <mukul.dharwadkar@gmail.com>
2024-11-07 11:29:19 -0800

    Event handler to select image on click

--- js/gallery.js
+++ js/gallery.js
@@ -12,11 +12,19 @@ const imgNames = ["../img/pic1.jpg", "../img/pic2.jpg", "../img/pic3.jpg", "../i
 const altTexts = ["pic1 alt text", "pic2 alt text", "pic3 alt text", "pic4 alt text", "pic5 alt text"]
 
 /* Looping through images */
+
+function selectImage() {
+    displayedImage.setAttribute("src", newImage.getAttribute("src"));
+    displayedImage.setAttribute("alt", newImage.getAttribute("alt"));
+}
+
 for (let i = 0; i < imgNames.length && i < altTexts.length; i++) {   
     const newImage = document.createElement('img');
     newImage.setAttribute('src', imgNames[i]);
     newImage.setAttribute('alt', altTexts[i]);
     thumbBar.appendChild(newImage);
+
+    newImage.addEventListener("click", selectImage);
 }
 
 /* Wiring up the Darken/Lighten button */
