bd2b6d69faad6c3a9996f58b407c3a7709f6796b
Mukul Dharwadkar <mukul.dharwadkar@gmail.com>
2024-11-12 12:28:12 -0800

    Hopefully working code.

--- js/objects.js
+++ js/objects.js
@@ -5,29 +5,41 @@
 */
 
 const team = document.querySelector(".member-name");
-const para = document.querySelector("p");
+// const para = document.querySelector("p");
 const teamSize = 11;
+let currentSize = 0;
 const btn = document.querySelector("button");
 
 function Person(name) {
     this.name = name;
     this.introduceSelf = function () {
-        team.textContent += name;
+        //team.textContent += name;
+        return this.name;
     }
 }
 
 function teamMember() {
-    const name = document.getElementsByClassName("member-name").value;
-    const member = new Person(name);
-    return member;
+    const name = document.querySelector("member-name").value;
+    console.log("Name is: ", name);
+    if (!name) return null;
+    return new Person(name);
 }
 
 function handleChange () {
-    let i = 0;
-    while (i < teamSize) {
-        const listItem = document.createElement("li");
-        listItem.textContent = `${i + 1}: ${teamMember()}`;
+    if (currentSize >= teamSize) {
+        alert("Team is full");
+        return;
     }
+
+    const member = teamMember();
+    if (!member) {
+        alert("Please enter a name");
+        return;
+    }
+
+    const listItem = document.createElement("li");
+    listItem.textContent = `${currentSize + 1}: ${member.introduceSelf()}`;
+    team.appendChild(listItem)
 }
 
 btn.addEventListener("click", handleChange);
