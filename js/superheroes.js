a2fbf8592aa503a09cfa538e2eb18611cf98b763
Mukul Dharwadkar <mukul.dharwadkar@gmail.com>
2024-11-18 12:20:11 -0800

    Completed function and minor CSS tweaks

--- js/superheroes.js
+++ js/superheroes.js
@@ -25,3 +25,39 @@ function populateHeader(obj) {
     myPara.textContent = `Hometow: ${obj.hometown} // Formed: ${obj.formed}`;
     header.appendChild(myPara)
 }
+
+function populateHeroes(obj) {
+    const section = document.querySelector("section");
+    const heroes = obj.members;
+
+    for (const hero of heroes) {
+        const myArticle = document.createElement("article");
+        const myH3 = document.createElement("h3");
+        const myPara1 = document.createElement("p");
+        const myPara2 = document.createElement("p");
+        const myPara3 = document.createElement("p");
+        const myList = document.createElement("ul");
+
+        myH3.textContent = hero.name;
+        myPara1.textContent = `Secret Identity: ${hero.secretIdentity}`;
+        myPara2.textContent = `Age: ${hero.age}`;
+        myPara3.textContent = "Superpowers:";
+
+        const superPowers = hero.powers;
+        for (const power of superPowers) {
+            const listItem = document.createElement("li");
+            listItem.textContent = power;
+            myList.appendChild(listItem);
+        }
+
+        myArticle.appendChild(myH3);
+        myArticle.appendChild(myPara1);
+        myArticle.appendChild(myPara2);
+        myArticle.appendChild(myPara3);
+        myArticle.appendChild(myList);
+
+        section.appendChild(myArticle);
+    }
+}
+
+populate();
