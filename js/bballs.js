788ae90ad7742733d89ecb0628cff4c21c869b70
Mukul Dharwadkar <mukul.dharwadkar@gmail.com>
2024-11-18 15:16:58 -0800

    Change height = width

--- js/bballs.js
+++ js/bballs.js
@@ -8,7 +8,7 @@ const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
 const width = (canvas.width = 900);
-const height = (canvas.height = 900);
+const height = (canvas.height = width);
 
 function random(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
