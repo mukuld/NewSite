5b31d05187146fe7fc5f5c2456e6e6a4900ca2e0
Mukul Dharwadkar <mukul.dharwadkar@gmail.com>
2024-11-19 13:30:09 -0800

    Removing Math.abs from update function to see the impact.

--- js/bballs-new.js
+++ js/bballs-new.js
@@ -37,19 +37,19 @@ class Ball {
 
     update() {
         if (this.x + this.size >= width) {
-            this.velX = -Math.abs(this.velX);
+            this.velX = -(this.velX);
         }
 
         if (this.x - this.size <= 0) {
-            this.velX = -Math.abs(this.velX);
+            this.velX = -(this.velX);
         }
 
         if (this.y + this.size >= height) {
-            this.velY = -Math.abs(this.velY);
+            this.velY = -(this.velY);
         }
 
         if (this.y - this.size <= 0) {
-            this.velY = -Math.abs(this.velY);
+            this.velY = -(this.velY);
         }
 
         this.x += this.velX;
