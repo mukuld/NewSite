7a28b1a21c0c5ce687c0468181d1d9f765bd3986
Mukul Dharwadkar <mukul.dharwadkar@gmail.com>
2024-12-06 10:47:36 -0800

    Setting window height to 500px

--- js/bballs-new.js
+++ js/bballs-new.js
@@ -10,7 +10,7 @@ const ctx = canvas.getContext("2d");
 // const body = document.body;
 
 const width = (canvas.width = window.innerWidth);
-const height = (canvas.height = window.innerHeight);
+const height = (canvas.height = "500px");
 
 function random(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
