bbc76c9c4cc0109ed28d084644f3376a2b1b9a10
Mukul Dharwadkar <mukul.dharwadkar@gmail.com>
2024-12-05 16:17:58 -0800

    setting lineWidth as an attribute not function

--- js/bballs-new.js
+++ js/bballs-new.js
@@ -111,7 +111,7 @@ class EvilCircle extends Shape {
 
     draw() {
         ctx.beginPath();
-        ctx.lineWidth(3);
+        ctx.lineWidth = 3;
         ctx.strokeStyle = this.color;
         ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
         ctx.stroke();
