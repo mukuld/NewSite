998060782996d2fe1fe62abc1da972a0ffe1ffd4
Mukul Dharwadkar <mukul.dharwadkar@gmail.com>
2024-12-30 23:32:41 -0800

    updated alarm code

--- js/alarm.js
+++ js/alarm.js
@@ -4,19 +4,21 @@
 * Programmer: Mukul Dharwadkar
 */
 
+const name = document.querySelector("#name");
+const delay = document.querySelector("#delay");
 const output = document.querySelector("#output");
 const button = document.querySelector("#set-alarm");
 
-function setAlarm() {
-    setTimeout(() => {
-        output.textContent = "Wake up!";
-    }, 1000);
-}
+// function setAlarm() {
+//     setTimeout(() => {
+//         output.textContent = "Wake up!";
+//     }, 1000);
+// }
 
 function alarm(person, delay) {
     return new Promise((resolve, reject) => {
         if (delay < 0) {
-            throw new Error("Alarm delay must not be negative");
+            reject(new Error("Alarm delay must not be negative"));
         }
         setTimeout(() => {
             resolve(`Wake up, ${person}!`);
@@ -24,4 +26,10 @@ function alarm(person, delay) {
     });
 }
 
-button.addEventListener("click", alarm);
+function handleClick() {
+    alarm(name.value, delay,value)
+    .then((message) => (output.textContent = message))
+    .catch((error) => (output.textContent = `Couldn't set alarm: ${error}`));
+}
+
+button.addEventListener("click", handleClick);
