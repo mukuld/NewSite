75ecca25b2d3a386d5e90ed0253c75f6701af443
Mukul Dharwadkar <mukul.dharwadkar@gmail.com>
2025-01-01 00:12:56 -0800

    Generate primes using web workers

---
+++ js/generate.js
@@ -0,0 +1,39 @@
+/* 
+* Javascript worker function to generate prime numbers
+* Date: 31 December 2024
+* Programmer: Mukul Dharwadkar
+*/
+
+/* Listen for messages from the main thread.
+If the message command is "generate", call the generatePrimes()*/
+
+addEventListener("message", (message) => {
+    if (message.data.command === "generate") {
+        generatePrimes(message.data.quota);
+    }
+});
+
+//Generate primes
+function generatePrimes() {
+    function isPrime(n) {
+        for (let c = 2; c <= Math.sqrt(n); ++c) {
+            if (n % c === 0) {
+                return false;
+            }
+        }
+        return true
+    }
+
+    const primes = [];
+    const maximum = 1000000;
+
+    while (primes.length < quota) {
+        const candidate = Math.floor(Math.random() * (maximum + 1));
+        if (isPrime(candidate)) {
+            primes.push(candidate);
+        }
+    }
+    /* When finished, send a message to the main thread,
+    including the number of primes we generated */
+    postMessage(primes.length)
+}
---
+++ js/primegen.js
@@ -0,0 +1,34 @@
+/* 
+* Javascript to generate prime numbers using workers
+* Date: 31 December 2024
+* Programmer: Mukul Dharwadkar
+*/
+
+// Create a new worker, giving it the code in generate.js by calling it here.
+const worker = new Worker("./generate.js");
+
+function handleClickToGenerate() {
+    const quota = document.querySelector("quota").value;
+    worker.postMessage({
+        command: "generate",
+        quota,
+    });
+}
+
+function handleClickToReload() {
+    document.querySelector("#user-input").value = 
+    'Try typing in here immediately after pressing "Generate primes"';
+    document.location.reload();
+}
+
+/* When the user clicks on Generate prime, send a message to the worker.
+The message command is "generate", and the message also contains "quota",
+which is the number of primes to be generated.*/
+document.querySelector("#generate").addEventListener(click, handleClickToGenerate);
+
+/* When the worker sends a message back to the main thread, update the output box with a message
+for the user, including the number of primes that were generated, taken from the message data */
+worker.addEventListener("message", (message) => {
+    document.querySelector("#output").textContent = 
+    `Finished generating ${message.data} primes!`;
+});
