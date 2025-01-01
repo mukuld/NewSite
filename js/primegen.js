58708f28770091a92b5c31021d05912572f0df6b
Mukul Dharwadkar <mukul.dharwadkar@gmail.com>
2025-01-01 00:17:39 -0800

    Quotes around click and reload code

--- js/primegen.js
+++ js/primegen.js
@@ -24,7 +24,7 @@ function handleClickToReload() {
 /* When the user clicks on Generate prime, send a message to the worker.
 The message command is "generate", and the message also contains "quota",
 which is the number of primes to be generated.*/
-document.querySelector("#generate").addEventListener(click, handleClickToGenerate);
+document.querySelector("#generate").addEventListener("click", handleClickToGenerate);
 
 /* When the worker sends a message back to the main thread, update the output box with a message
 for the user, including the number of primes that were generated, taken from the message data */
@@ -32,3 +32,5 @@ worker.addEventListener("message", (message) => {
     document.querySelector("#output").textContent = 
     `Finished generating ${message.data} primes!`;
 });
+
+document.querySelector("#reload").addEventListener("click", handleClickToReload);
