82645eb0054cdbd91cbb3e3e1dce8fd0041fe43f
Mukul Dharwadkar <mukul.dharwadkar@gmail.com>
2025-01-06 13:30:34 -0800

    Initial Alice Commit

---
+++ js/alice.js
@@ -0,0 +1,20 @@
+/* 
+* Javascript to sequence animations
+* Date: 06 January 2025
+* Programmer: Mukul Dharwadkar
+*/
+
+const aliceTumbling = [
+    { transform: 'rotate(0) scale(1)' },
+    { transform: 'rotate(360deg) scale(0)' }
+  ];
+  
+  const aliceTiming = {
+    duration: 2000,
+    iterations: 1,
+    fill: 'forwards'
+  }
+  
+  const alice1 = document.querySelector("#alice1");
+  const alice2 = document.querySelector("#alice2");
+  const alice3 = document.querySelector("#alice3");
